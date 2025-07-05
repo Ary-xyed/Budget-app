e7c06422feb4d0ede1f945fcd65858344f1fcbeb
Ary-xyed <ary.xyed@gmail.com>
2025-07-05 00:20:47 -0400

    Fix bar chart month calculation and add python-dateutil

--- app.py
+++ app.py
@@ -308,4 +308,5 @@ def create_tables_and_seed():
         seeded = True
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port) 
